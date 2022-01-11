55492444151902939704728aa40142ac1d84df86
Will Xu <xwywryx@outlook.com>
2022-01-10 19:37:38 -0800

    :sparkles: 929. Unique Email Addresses

---
+++ src/main/java/org/willxu/algorithm/service/impl/integer/UniqueEmailAddressesLoop.java
@@ -0,0 +1,42 @@
+package org.willxu.algorithm.service.impl.integer;
+
+import org.willxu.algorithm.service.integer.UniqueEmailAddresses;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmailAddressesLoop implements UniqueEmailAddresses {
+    /**
+     * 1. Define boolean split: has meet '@' or not.
+     * 2. Define boolean ignore: has meet first '+', before '@'.
+     * 3. Before split, loop the chars, remove '.'
+     * 4. Add emails to set.
+     */
+    @Override
+    public int numUniqueEmails(String[] emails) {
+        Set<String> emailsSet = new HashSet<>();
+        for (String email: emails) {
+            boolean split = false;
+            boolean ignore = false;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (char c: email.toCharArray()) {
+                if (split) {
+                    stringBuilder.append(c);
+                } else {
+                    if ('@' == c) {
+                        stringBuilder.append(c);
+                        split = true;
+                    } else if (!ignore) {
+                        if ('+' == c) {
+                            ignore = true;
+                        } else if ('.' != c) {
+                            stringBuilder.append(c);
+                        }
+                    }
+                }
+            }
+            emailsSet.add(stringBuilder.toString());
+        }
+        return emailsSet.size();
+    }
+}
---
+++ src/test/java/org/willxu/algorithm/service/integer/UniqueEmailAddressesTest.java
@@ -0,0 +1,49 @@
+package org.willxu.algorithm.service.integer;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.integer.UniqueEmailAddressesLoop;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class UniqueEmailAddressesTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                // 1: Forgot add '@'.
+                arguments(
+                        new String[] {
+                                "test.email+alex@leetcode.com",
+                                "test.email.leet+alex@code.com"
+                        },
+                        2
+                ),
+                arguments(
+                        new String[] {
+                                "test.email+alex@leetcode.com",
+                                "test.e.mail+bob.cathy@leetcode.com",
+                                "testemail+david@lee.tcode.com"
+                        },
+                        2
+                ),
+                arguments(
+                        new String[] {
+                                "a@leetcode.com",
+                                "b@leetcode.com",
+                                "c@leetcode.com"
+                        },
+                        3
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Loop {index}")
+    @MethodSource("dataProvider")
+    public void testLoop(String[] emails, int excepted) {
+        UniqueEmailAddresses loop = new UniqueEmailAddressesLoop();
+        assertEquals(excepted, loop.numUniqueEmails(emails));
+    }
+}
