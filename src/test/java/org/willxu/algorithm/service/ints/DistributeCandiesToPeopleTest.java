199734756a39b293ea90c94cb146b33827c7a053
Will Xu <xwywryx@outlook.com>
2022-02-11 20:28:22 -0800

    :sparkles: 1103. Distribute Candies to People

---
+++ src/test/java/org/willxu/algorithm/service/ints/DistributeCandiesToPeopleTest.java
@@ -0,0 +1,29 @@
+package org.willxu.algorithm.service.ints;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.ints.DistributeCandiesToPeopleLoop;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class DistributeCandiesToPeopleTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                // 1,0,0,0 -> 1,2,0,0 -> 1,2,3,0 -> 1,2,3,1
+                arguments(7, 4, new int[] {1, 2, 3, 1}),
+                // 1,0,0 -> 1,2,0 -> 1,2,3 -> 5,2,3
+                arguments(10, 3, new int[] {5, 2, 3})
+        );
+    }
+
+    @ParameterizedTest(name = "Loop {index}")
+    @MethodSource("dataProvider")
+    public void testLoop(int candies, int num, int[] excepted) {
+        DistributeCandiesToPeople loop = new DistributeCandiesToPeopleLoop();
+        assertArrayEquals(excepted, loop.distributeCandies(candies, num));
+    }
+}
