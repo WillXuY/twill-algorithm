2c55c337514df6c51ce08cf05c1c62521cb1652a
Will Xu <xwywryx@outlook.com>
2022-02-24 21:21:17 +0800

    :sparkles: 1207. Unique Number of Occurrences

---
+++ src/main/java/org/willxu/algorithm/service/bool/UniqueNumberOfOccurrences.java
@@ -0,0 +1,14 @@
+package org.willxu.algorithm.service.bool;
+
+public interface UniqueNumberOfOccurrences {
+    /**
+     * Given an array of integers arr, return true if the number of
+     * occurrences of each value in the array is unique, or false
+     * otherwise.
+     *
+     * @param arr 1 <= arr.length <= 1000
+     *            -1000 <= arr[i] <= 1000
+     * @return the number of occurrences is unique.
+     */
+    boolean uniqueOccurrences(int[] arr);
+}
---
+++ src/test/java/org/willxu/algorithm/service/bool/UniqueNumberOfOccurrencesTest.java
@@ -0,0 +1,28 @@
+package org.willxu.algorithm.service.bool;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.bool.UniqueNumberOfOccurrencesCount;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class UniqueNumberOfOccurrencesTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(new int[] {1, 2, 2, 1, 1, 3}, true),
+                arguments(new int[] {1, 2}, false),
+                arguments(new int[] {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0}, true)
+        );
+    }
+
+    @ParameterizedTest(name = "Count {index}")
+    @MethodSource("dataProvider")
+    public void testCount(int[] arr, boolean excepted) {
+        UniqueNumberOfOccurrences count = new UniqueNumberOfOccurrencesCount();
+        assertEquals(excepted, count.uniqueOccurrences(arr));
+    }
+}
