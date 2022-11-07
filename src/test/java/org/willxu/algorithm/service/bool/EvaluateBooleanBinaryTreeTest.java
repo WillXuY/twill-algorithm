614f82c98a15c6eb7270eb1abff730d73d426702
Will Xu <xwywryx@outlook.com>
2022-11-07 22:12:38 +0800

    :sparkles: 2331. Evaluate Boolean Binary Tree

---
+++ src/test/java/org/willxu/algorithm/service/bool/EvaluateBooleanBinaryTreeTest.java
@@ -0,0 +1,40 @@
+package org.willxu.algorithm.service.bool;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.impl.bool.EvaluateBooleanBinaryTreeRecursice;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.gradle.internal.impldep.org.junit.Assert.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class EvaluateBooleanBinaryTreeTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                2, 1, 3, null, null, 0, 1
+                        ).collect(Collectors.toList())),
+                        true
+                ),
+                arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                0
+                        ).collect(Collectors.toList())),
+                        false
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Recursive {index}")
+    @MethodSource("dataProvider")
+    public void testRecursive(TreeNode root, boolean excepted) {
+        EvaluateBooleanBinaryTree recursive =
+                new EvaluateBooleanBinaryTreeRecursice();
+        assertEquals(excepted, recursive.evaluateTree(root));
+    }
+}
