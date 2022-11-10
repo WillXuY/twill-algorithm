c5a7e4f465d8abd34924af356325246c67b7f3a5
Will Xu <xwywryx@outlook.com>
2022-11-10 21:02:10 +0800

    :bug: import error method "assertEquals".

--- src/test/java/org/willxu/algorithm/service/doublepoint/CalculateAmountPaidInTaxesTest.java
+++ src/test/java/org/willxu/algorithm/service/doublepoint/CalculateAmountPaidInTaxesTest.java
@@ -7,7 +7,7 @@ import org.willxu.algorithm.service.impl.doublepoint.CalculateAmountPaidInTaxesL
 
 import java.util.stream.Stream;
 
-import static org.gradle.internal.impldep.org.testng.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CalculateAmountPaidInTaxesTest {
@@ -23,6 +23,6 @@ public class CalculateAmountPaidInTaxesTest {
     @MethodSource("dataProvider")
     public void testLoop(int[][] brackets, int income, double excepted) {
         CalculateAmountPaidInTaxes loop = new CalculateAmountPaidInTaxesLoop();
-        assertEquals(loop.calculateTax(brackets, income), excepted);
+        assertEquals(excepted, loop.calculateTax(brackets, income));
     }
 }
