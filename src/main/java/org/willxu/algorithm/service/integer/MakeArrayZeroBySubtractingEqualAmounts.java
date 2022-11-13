7d334544f43cb817d96b337f89c8462ca1aa3423
Will Xu <xwywryx@outlook.com>
2022-11-13 05:33:45 -0800

    :sparkles: 2357. Make Array Zero by Subtracting Equal Amounts

---
+++ src/main/java/org/willxu/algorithm/service/integer/MakeArrayZeroBySubtractingEqualAmounts.java
@@ -0,0 +1,19 @@
+package org.willxu.algorithm.service.integer;
+
+public interface MakeArrayZeroBySubtractingEqualAmounts {
+    /**
+     * You are given a non-negative integer array nums. In one
+     * operation, you must:
+     * <p>
+     * - Choose a positive integer x such that x is less than or equal
+     *   to the smallest non-zero element in nums.
+     * - Subtract x from every positive element in nums.
+     * <p>
+     * Return the minimum number of operations to make every element in
+     * nums equal to 0.
+     *
+     * @param nums 1 <= nums.length <= 100
+     *             0 <= nums[i] <= 100
+     */
+    int minimumOperations(int[] nums);
+}
---
+++ src/test/java/org/willxu/algorithm/service/integer/MakeArrayZeroBySubtractingEqualAmountsTest.java
@@ -0,0 +1,29 @@
+package org.willxu.algorithm.service.integer;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.integer.MakeArrayZeroBySubtractingEqualAmountsSet;
+
+import java.util.stream.Stream;
+
+import static org.gradle.internal.impldep.org.junit.Assert.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class MakeArrayZeroBySubtractingEqualAmountsTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(new int[] {1}, 1),
+                arguments(new int[] {1, 5, 0, 3, 5}, 3),
+                arguments(new int[] {0}, 0)
+        );
+    }
+
+    @ParameterizedTest(name = "Set {index}")
+    @MethodSource("dataProvider")
+    public void testSet(int[] nums, int excepted) {
+        MakeArrayZeroBySubtractingEqualAmounts set =
+                new MakeArrayZeroBySubtractingEqualAmountsSet();
+        assertEquals(excepted, set.minimumOperations(nums));
+    }
+}
