11f94d7ebd962208260595b666196cd2d8df5137
Will Xu <xwywryx@outlook.com>
2022-12-23 09:53:20 +0800

    :sparkles: 2496. Maximum Value of a String in an Array

---
+++ src/main/java/org/willxu/algorithm/service/integer/MaximumValueOfStringInAnArray.java
@@ -0,0 +1,20 @@
+package org.willxu.algorithm.service.integer;
+
+public interface MaximumValueOfStringInAnArray {
+    /**
+     * The value of an alphanumeric string can be defined as:
+     * <p>
+     * - The numeric representation of the string in base 10, if it
+     *   comprises of digits only.
+     * - The length of the string, otherwise.
+     * <p>
+     * Given an array strs of alphanumeric strings, return the maximum
+     * value of any string in strs.
+     *
+     * @param strs 1 <= strs.length <= 100
+     *             1 <= strs[i].length <= 9
+     *             strs[i] consists of only lowercase English letters
+     *             and digits.
+     */
+    int maximumValue(String[] strs);
+}
