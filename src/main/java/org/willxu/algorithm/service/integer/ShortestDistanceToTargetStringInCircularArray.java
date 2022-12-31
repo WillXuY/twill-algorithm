18914d5846359817ae7499c86ec02fb46d5489e1
Will Xu <xwywryx@outlook.com>
2023-01-01 00:43:21 +0800

    :todo: 2515. Shortest Distance to Target String in a Circular Array

---
+++ src/main/java/org/willxu/algorithm/service/integer/ShortestDistanceToTargetStringInCircularArray.java
@@ -0,0 +1,26 @@
+package org.willxu.algorithm.service.integer;
+
+public interface ShortestDistanceToTargetStringInCircularArray {
+    /**
+     * You are given a 0-indexed circular string array words and a
+     * string target. A circular array means that the array's end
+     * connects to the array's beginning.
+     * <p>
+     * - Formally, the next element of words[i] is words[(i + 1) % n]
+     *   and the previous element of words[i] is words[(i - 1 + n) % n],
+     *   where n is the length of words.
+     * <p>
+     * Starting from startIndex, you can move to either the next word or
+     * the previous word with 1 step at a time.
+     * <p>
+     * Return the shortest distance needed to reach the string target.
+     * If the string target does not exist in words, return -1.
+     *
+     * @param words 1 <= words.length <= 100
+     *              1 <= words[i].length <= 100
+     *              consist of only lowercase English letters.
+     * @param target consist of only lowercase English letters.
+     * @param startIndex 0 <= startIndex < words.length
+     */
+    int closetTarget(String[] words, String target, int startIndex);
+}
