0cbedb1a8d1ef77819926103501300413f70040a
Will Xu <xwywryx@outlook.com>
2023-01-24 23:48:43 +0800

    :sparkles: 31. Next Permutation

---
+++ src/main/java/org/willxu/algorithm/service/impl/empty/NextPermutationLoop.java
@@ -0,0 +1,47 @@
+package org.willxu.algorithm.service.impl.empty;
+
+import org.willxu.algorithm.service.empty.NextPermutation;
+
+import java.util.Arrays;
+
+public class NextPermutationLoop implements NextPermutation {
+    @Override
+    public void nextPermutation(int[] nums) {
+        int firstDecreasing = 0;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                firstDecreasing = i - 1;
+                break;
+            }
+            if (i == 1) {
+                Arrays.sort(nums);
+                return;
+            }
+        }
+        int exchange = 0;
+        for (int i = firstDecreasing + 1; i < nums.length; i++) {
+            if (nums[i] <= nums[firstDecreasing]) {
+                exchange = i - 1;
+                break;
+            }
+            if (i == nums.length - 1) {
+                exchange = nums.length - 1;
+            }
+        }
+        if (firstDecreasing == exchange) {
+            return;
+        }
+        nums[firstDecreasing] = nums[firstDecreasing] ^ nums[exchange];
+        nums[exchange] = nums[firstDecreasing] ^ nums[exchange];
+        nums[firstDecreasing] = nums[firstDecreasing] ^ nums[exchange];
+        int order = firstDecreasing + 1;
+        int last = nums.length - 1;
+        while (order < last) {
+            nums[order] = nums[order] ^ nums[last];
+            nums[last] = nums[order] ^ nums[last];
+            nums[order] = nums[order] ^ nums[last];
+            order++;
+            last--;
+        }
+    }
+}
--- src/test/java/org/willxu/algorithm/service/empty/NextPermutationTest.java
+++ src/test/java/org/willxu/algorithm/service/empty/NextPermutationTest.java
@@ -13,6 +13,9 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class NextPermutationTest {
     static Stream<Arguments> dataProvider() {
         return Stream.of(
+                arguments(new int[] {1, 5, 1}, new int[] {5, 1, 1}),
+                arguments(new int[] {2, 3, 1}, new int[] {3, 1, 2}),
+                arguments(new int[] {1}, new int[] {1}),
                 arguments(new int[] {1, 3, 2}, new int[] {2, 1, 3}),
                 arguments(new int[] {1, 2, 3}, new int[] {1, 3, 2}),
                 arguments(new int[] {3, 2, 1}, new int[] {1, 2, 3}),
