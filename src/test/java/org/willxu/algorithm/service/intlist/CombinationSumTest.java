dc9b80df15430ca0e3739853e0779c79db70219c
Will Xu <xwywryx@outlook.com>
2023-02-04 23:55:30 +0800

    :sparkles: 39. Combination Sum

--- src/test/java/org/willxu/algorithm/service/intlist/CombinationSumTest.java
+++ src/test/java/org/willxu/algorithm/service/intlist/CombinationSumTest.java
@@ -1,11 +1,18 @@
 package org.willxu.algorithm.service.intlist;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.intlist.CombinationSumRecursive;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class CombinationSumTest {
@@ -38,5 +45,17 @@ public class CombinationSumTest {
                 ),
                 arguments(new int[] {2}, 1, Collections.emptyList())
         );
+    }
+
+    @ParameterizedTest(name = "Recursive {index}")
+    @MethodSource("dataProvider")
+    public void testRecursive(int[] candidates, int target,
+                              List<List<Integer>> excepted)
+            throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        CombinationSum recursive = new CombinationSumRecursive();
+        String output = objectMapper.writeValueAsString(
+                recursive.combinationSum(candidates, target));
+        assertEquals(objectMapper.writeValueAsString(excepted), output);
     }
 }
