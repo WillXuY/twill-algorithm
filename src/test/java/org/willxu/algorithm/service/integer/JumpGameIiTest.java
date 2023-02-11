79c6e4092bc55f430250fc7a21376c9b430ed5a0
Will Xu <xwywryx@outlook.com>
2023-02-11 17:54:44 +0800

    :TimeLimitExceeded: 45. Jump Game II

---
+++ src/test/java/org/willxu/algorithm/service/integer/JumpGameIiTest.java
@@ -0,0 +1,28 @@
+package org.willxu.algorithm.service.integer;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.integer.JumpGameIiLoop;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class JumpGameIiTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(new int[] {0}, 0),
+                arguments(new int[] {2, 3, 1, 1, 4}, 2),
+                arguments(new int[] {2, 3, 0, 1, 4}, 2)
+        );
+    }
+
+    @ParameterizedTest(name = "Loop {index}")
+    @MethodSource("dataProvider")
+    public void testLoop(int[] nums, int excepted) {
+        JumpGameIi loop = new JumpGameIiLoop();
+        assertEquals(excepted, loop.jump(nums));
+    }
+}
