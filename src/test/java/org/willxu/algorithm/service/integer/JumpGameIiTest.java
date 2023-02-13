30949d85ac6f98e0c4656481447e9a7ed78e1cfc
Will Xu <xwywryx@outlook.com>
2023-02-13 23:19:44 +0800

    :sparkles: 45. Jump Game II

--- src/test/java/org/willxu/algorithm/service/integer/JumpGameIiTest.java
+++ src/test/java/org/willxu/algorithm/service/integer/JumpGameIiTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.willxu.algorithm.service.impl.integer.JumpGameIiLoop;
 import org.willxu.algorithm.service.impl.integer.JumpGameIiRange;
+import org.willxu.algorithm.service.impl.integer.JumpGameIiSkip;
 
 import java.util.stream.Stream;
 
@@ -14,10 +15,21 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 public class JumpGameIiTest {
     static Stream<Arguments> dataProvider() {
         return Stream.of(
+                arguments(new int[] {1, 1, 1, 1}, 3),
+                arguments(new int[] {1, 2}, 1),
+                arguments(new int[] {2, 3, 1, 1, 4}, 2),
+                arguments(new int[] {1, 2, 3}, 2),
                 arguments(new int[] {0}, 0),
                 arguments(new int[] {2, 3, 1, 1, 4}, 2),
                 arguments(new int[] {2, 3, 0, 1, 4}, 2)
         );
+    }
+
+    @ParameterizedTest(name = "Skip {index}")
+    @MethodSource("dataProvider")
+    public void testSkip(int[] nums, int excepted) {
+        JumpGameIi skip = new JumpGameIiSkip();
+        assertEquals(excepted, skip.jump(nums));
     }
 
     @ParameterizedTest(name = "Range {index}")
