ab3dec56fc46ab669d1c6bb9fb2cb9fb716918c0
Will Xu <xwywryx@outlook.com>
2023-02-26 14:44:13 +0800

    :sparkles: 59. Spiral Matrix II

---
+++ src/main/java/org/willxu/algorithm/service/matrix/SpiralMatrixIi.java
@@ -0,0 +1,11 @@
+package org.willxu.algorithm.service.matrix;
+
+public interface SpiralMatrixIi {
+    /**
+     * Given a positive integer n, generate an n x n matrix filled with
+     * elements from 1 to n^2 in spiral order.
+     *
+     * @param n 1 <= n <= 20
+     */
+    int[][] generateMatrix(int n);
+}
---
+++ src/test/java/org/willxu/algorithm/service/matrix/SpiralMatrixIiTest.java
@@ -0,0 +1,34 @@
+package org.willxu.algorithm.service.matrix;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.matrix.SpiralMatrixIiEdge;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class SpiralMatrixIiTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(
+                        3,
+                        new int[][] {
+                                {1, 2, 3},
+                                {8, 9, 4},
+                                {7, 6, 5}
+                        }
+                ),
+                arguments(1, new int[][] {{1}})
+        );
+    }
+
+    @ParameterizedTest(name = "Edge {index}")
+    @MethodSource("dataProvider")
+    public void testEdge(int n, int[][] excepted) {
+        SpiralMatrixIi edge = new SpiralMatrixIiEdge();
+        assertArrayEquals(excepted, edge.generateMatrix(n));
+    }
+}
