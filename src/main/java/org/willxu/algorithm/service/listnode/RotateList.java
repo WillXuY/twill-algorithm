64e0b5add8d6d1b7a966ef4a369f4d81636c78fc
Will Xu <xwywryx@outlook.com>
2023-02-27 15:15:31 +0800

    :sparkles: 61. Rotate List

---
+++ src/main/java/org/willxu/algorithm/service/listnode/RotateList.java
@@ -0,0 +1,16 @@
+package org.willxu.algorithm.service.listnode;
+
+import org.willxu.algorithm.provide.ListNode;
+
+public interface RotateList {
+    /**
+     * Given the head of a linked list, rotate the list to the right
+     * by k places.
+     *
+     * @param head The number of nodes in the list is in the
+     *             range [0, 500].
+     *             -100 <= Node.val <= 100
+     * @param k 0 <= k <= 2 * 109
+     */
+    ListNode rotateRight(ListNode head, int k);
+}
---
+++ src/test/java/org/willxu/algorithm/service/listnode/RotateListTest.java
@@ -0,0 +1,104 @@
+package org.willxu.algorithm.service.listnode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.provide.ListNode;
+import org.willxu.algorithm.service.impl.listnode.RotateListPointer;
+
+import java.util.stream.Stream;
+
+import static org.gradle.internal.impldep.org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class RotateListTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1, 2, 3, 4, 5}
+                        ),
+                        10,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1, 2, 3, 4, 5}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1}
+                        ),
+                        99,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1, 2}
+                        ),
+                        2,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1, 2}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1}
+                        ),
+                        1,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1}
+                        ),
+                        0,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {}
+                        ),
+                        0,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {1, 2, 3, 4, 5}
+                        ),
+                        2,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {4, 5, 1, 2, 3}
+                        )
+                ),
+                arguments(
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {0, 1, 2}
+                        ),
+                        4,
+                        ListNode.getListNodeFromIntArray(
+                                new int[] {2, 0, 1}
+                        )
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Pointer {index}")
+    @MethodSource("dataProvider")
+    public void testPointer(ListNode head, int k, ListNode excepted) {
+        RotateList pointer = new RotateListPointer();
+        ListNode output = pointer.rotateRight(head, k);
+        if (excepted == null) {
+            assertNull(output);
+        } else {
+            assertArrayEquals(excepted.toIntArray(), output.toIntArray());
+        }
+    }
+}
