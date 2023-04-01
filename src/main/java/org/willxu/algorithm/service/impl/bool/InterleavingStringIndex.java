aac390e80a123d7100fcefdcf4a8aaa54abc43af
Will Xu <xwywryx@outlook.com>
2023-04-01 23:12:16 +0800

    :sparkles: 97. Interleaving String index String.

--- src/main/java/org/willxu/algorithm/service/impl/bool/InterleavingStringIndex.java
+++ src/main/java/org/willxu/algorithm/service/impl/bool/InterleavingStringIndex.java
@@ -11,24 +11,26 @@ public class InterleavingStringIndex implements InterleavingString {
         if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
-        Set<int[]> possibleIndex = new HashSet<>();
-        int[] start = new int[] {-1, -1};
-        possibleIndex.add(start);
+        Set<String> possibleIndex = new HashSet<>();
+        possibleIndex.add("-1,-1");
         for (char c: s3.toCharArray()) {
-            Set<int[]> newPossibleIndex = new HashSet<>();
-            for (int[] possible: possibleIndex) {
-                int index1 = possible[0] + 1;
+            Set<String> newPossibleIndex = new HashSet<>();
+            for (String possible: possibleIndex) {
+                String[] p = possible.split(",");
+                int lastIndex1 = Integer.parseInt(p[0]);
+                int lastIndex2 = Integer.parseInt(p[1]);
+                int index1 = lastIndex1 + 1;
                 if (index1 < s1.length()) {
                     char c1 = s1.charAt(index1);
                     if (c1 == c) {
-                        newPossibleIndex.add(new int[] {index1, possible[1]});
+                        newPossibleIndex.add(index1 + "," + lastIndex2);
                     }
                 }
-                int index2 = possible[1] + 1;
+                int index2 = lastIndex2 + 1;
                 if (index2 < s2.length()) {
                     char c2 = s2.charAt(index2);
                     if (c2 == c) {
-                        newPossibleIndex.add(new int[] {possible[0], index2});
+                        newPossibleIndex.add(lastIndex1 + "," + index2);
                     }
                 }
             }
