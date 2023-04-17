04059041034bbb7267ac350c681f71b0cafd6cae
Will Xu <xwywryx@outlook.com>
2023-04-17 22:51:28 +0800

    :sparkles: 129. Sum Root to Leaf Numbers

---
+++ src/test/java/org/willxu/algorithm/service/integer/SumRootToLeafNumbersTest.java
@@ -0,0 +1,38 @@
+package org.willxu.algorithm.service.integer;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.impl.integer.SumRootToLeafNumbersLevel;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumRootToLeafNumbersTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                1, 2, 3
+                        ).collect(Collectors.toList())),
+                        25
+                ),
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                4, 9, 0, 5, 1
+                        ).collect(Collectors.toList())),
+                        1026
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Level {index}")
+    @MethodSource("dataProvider")
+    public void testLevel(TreeNode root, int excepted) {
+        SumRootToLeafNumbers level = new SumRootToLeafNumbersLevel();
+        assertEquals(excepted, level.sumNumbers(root));
+    }
+}
