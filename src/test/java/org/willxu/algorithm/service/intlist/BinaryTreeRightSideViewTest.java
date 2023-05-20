03596b9cf20ad0fae411deae89527c77641507ec
Will Xu <xwywryx@outlook.com>
2023-05-20 21:17:21 +0800

    :sparkles: 199. Binary Tree Right Side View

---
+++ src/test/java/org/willxu/algorithm/service/intlist/BinaryTreeRightSideViewTest.java
@@ -0,0 +1,48 @@
+package org.willxu.algorithm.service.intlist;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.impl.intlist.BinaryTreeRightSideViewLevel;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinaryTreeRightSideViewTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                1, 2, 3, null, 5, null, 4
+                        ).collect(Collectors.toList())),
+                        Stream.of(
+                                1, 3, 4
+                        ).collect(Collectors.toList())
+                ),
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                1, null, 3
+                        ).collect(Collectors.toList())),
+                        Stream.of(
+                                1, 3
+                        ).collect(Collectors.toList())
+                ),
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Collections.emptyList()),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Level {index}")
+    @MethodSource("dataProvider")
+    public void testLevel(TreeNode root, List<Integer> excepted) {
+        BinaryTreeRightSideView level = new BinaryTreeRightSideViewLevel();
+        assertEquals(excepted, level.rightSideView(root));
+    }
+}
