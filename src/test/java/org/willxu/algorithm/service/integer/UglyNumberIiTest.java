e936e6dc920ca8b5f8e3bdb15b9521f42d44215e
Will Xu <xwywryx@outlook.com>
2023-06-09 18:12:57 +0800

    :sparkles: 264. Ugly Number II

---
+++ src/test/java/org/willxu/algorithm/service/integer/UglyNumberIiTest.java
@@ -0,0 +1,26 @@
+package org.willxu.algorithm.service.integer;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.integer.UglyNumberIiMin;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UglyNumberIiTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.arguments(10, 12),
+                Arguments.arguments(1, 1)
+        );
+    }
+
+    @ParameterizedTest(name = "Min {index}")
+    @MethodSource("dataProvider")
+    public void testMin(int n, int excepted) {
+        UglyNumberIi min = new UglyNumberIiMin();
+        assertEquals(excepted, min.nthUglyNumber(n));
+    }
+}
