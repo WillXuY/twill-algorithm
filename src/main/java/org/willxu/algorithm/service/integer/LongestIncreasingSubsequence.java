d2173bfbd6b4d5d653df870f9db9895650d77770
Will Xu <xwywryx@outlook.com>
2023-06-18 22:34:35 +0800

    :sparkles: 300. Longest Increasing Subsequence

---
+++ src/main/java/org/willxu/algorithm/service/integer/LongestIncreasingSubsequence.java
@@ -0,0 +1,12 @@
+package org.willxu.algorithm.service.integer;
+
+public interface LongestIncreasingSubsequence {
+    /**
+     * Given an integer array nums, return the length of the longest
+     * strictly increasing subsequence.
+     *
+     * @param nums 1 <= nums.length <= 2500
+     *             -10^4 <= nums[i] <= 10^4
+     */
+    int lengthOfLIS(int[] nums);
+}
