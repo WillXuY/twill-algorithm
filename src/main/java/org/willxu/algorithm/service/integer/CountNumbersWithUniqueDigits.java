bc82c098d95df2aeec8819d40539e779a80c458b
Will Xu <xwywryx@outlook.com>
2023-07-10 20:38:12 +0800

    :sparkles: 357. Count Numbers with Unique Digits

---
+++ src/main/java/org/willxu/algorithm/service/integer/CountNumbersWithUniqueDigits.java
@@ -0,0 +1,11 @@
+package org.willxu.algorithm.service.integer;
+
+public interface CountNumbersWithUniqueDigits {
+    /**
+     * Given an integer n, return the count of all numbers with unique
+     * digits, x, where 0 <= x < 10^n.
+     *
+     * @param n 0 <= n <= 8
+     */
+    int countNumbersWithUniqueDigits(int n);
+}
