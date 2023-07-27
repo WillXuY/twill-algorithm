d2770e3a1a5dc9d312d040f4afd3eaa8fb1df25c
Will Xu <xwywryx@outlook.com>
2023-07-27 22:51:43 +0800

    :sparkles: 393. UTF-8 Validation

---
+++ src/main/java/org/willxu/algorithm/service/bool/Utf8Validation.java
@@ -0,0 +1,39 @@
+package org.willxu.algorithm.service.bool;
+
+public interface Utf8Validation {
+    /**
+     * Given an integer array data representing the data, return whether
+     * it is a valid UTF-8 encoding (i.e. it translates to a sequence of
+     * valid UTF-8 encoded characters).
+     * <p>
+     * A character in UTF8 can be from 1 to 4 bytes long, subjected to
+     * the following rules:
+     * <p>
+     * 1. For a 1-byte character, the first bit is a 0, followed by its
+     *    Unicode code.
+     * 2. For an n-bytes character, the first n bits are all one's, the
+     *    n + 1 bit is 0, followed by n - 1 bytes with the most
+     *    significant 2 bits being 10.
+     * <p>
+     * This is how the UTF-8 encoding would work:
+     * <p>
+     *      Number of Bytes   |        UTF-8 Octet Sequence
+     *                        |              (binary)
+     *    --------------------+-----------------------------------------
+     *             1          |   0xxxxxxx
+     *             2          |   110xxxxx 10xxxxxx
+     *             3          |   1110xxxx 10xxxxxx 10xxxxxx
+     *             4          |   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+     * <p>
+     * x denotes a bit in the binary form of a byte that may be either
+     * 0 or 1.
+     * <p>
+     * Note: The input is an array of integers. Only the least
+     * significant 8 bits of each integer is used to store the data.
+     * This means each integer represents only 1 byte of data.
+     *
+     * @param data 1 <= data.length <= 2 * 10^4
+     *             0 <= data[i] <= 255
+     */
+    boolean validUtf8(int[] data);
+}
