2ac10d6b057ecf190709410ae42afe14be6c5ec0
Will Xu <xwywryx@outlook.com>
2023-08-03 21:53:07 +0800

    :sparkles: 400. Nth Digit

---
+++ src/main/java/org/willxu/algorithm/service/integer/NthDigit.java
@@ -0,0 +1,11 @@
+package org.willxu.algorithm.service.integer;
+
+public interface NthDigit {
+    /**
+     * Given an integer n, return the nth digit of the infinite integer
+     * sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
+     *
+     * @param n 1 <= n <= 2^31 - 1
+     */
+    int findNthDigit(int n);
+}
