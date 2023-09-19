9f0cedf4ae361ad8e113981a3e954ab0c715f2de
Will Xu <xwywryx@outlook.com>
2023-09-19 22:40:34 +0800

    :sparkles: 508. Most Frequent Subtree Sum

---
+++ src/main/java/org/willxu/algorithm/service/impl/ints/MostFrequentSubtreeSumMap.java
@@ -0,0 +1,77 @@
+package org.willxu.algorithm.service.impl.ints;
+
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.ints.MostFrequentSubtreeSum;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MostFrequentSubtreeSumMap implements MostFrequentSubtreeSum {
+    @Override
+    public int[] findFrequentTreeSum(TreeNode root) {
+        Map<Integer, Integer> sumCountMap = recursive(root);
+        sumCountMap.remove(Integer.MIN_VALUE);
+        int maxSum = 0;
+        Set<Integer> sumSet = new HashSet<>();
+        for (int key: sumCountMap.keySet()) {
+            int val = sumCountMap.get(key);
+            if (maxSum < val) {
+                maxSum = val;
+                sumSet.clear();
+                sumSet.add(key);
+            } else if (maxSum == val) {
+                sumSet.add(key);
+            }
+        }
+        int[] result = new int[sumSet.size()];
+        int i = 0;
+        for (int s: sumSet) {
+            result[i] = s;
+            i++;
+        }
+        return result;
+    }
+
+    /**
+     * @return sum's count and key Integer.MIN_VALUE's value is tree sum.
+     */
+    private Map<Integer, Integer> recursive(TreeNode node) {
+        Map<Integer, Integer> sumCountMap = new HashMap<>();
+        if (node == null) {
+            return sumCountMap;
+        } else if (node.left == null && node.right == null) {
+            sumCountMap.put(node.val, 1);
+            sumCountMap.put(Integer.MIN_VALUE, node.val);
+            return sumCountMap;
+        }
+        Map<Integer, Integer> leftMap = recursive(node.left);
+        Map<Integer, Integer> rightMap = recursive(node.right);
+        int treeSum = node.val;
+        if (!leftMap.isEmpty()) {
+            treeSum += leftMap.get(Integer.MIN_VALUE);
+        }
+        if (!rightMap.isEmpty()) {
+            treeSum += rightMap.get(Integer.MIN_VALUE);
+        }
+        leftMap.remove(Integer.MIN_VALUE);
+        rightMap.remove(Integer.MIN_VALUE);
+        sumCountMap.putAll(leftMap);
+        for (int key: rightMap.keySet()) {
+            int val = rightMap.get(key);
+            if (sumCountMap.containsKey(key)) {
+                sumCountMap.put(key, sumCountMap.get(key) + val);
+            } else {
+                sumCountMap.put(key, val);
+            }
+        }
+        if (sumCountMap.containsKey(treeSum)) {
+            sumCountMap.put(treeSum, sumCountMap.get(treeSum) + 1);
+        } else {
+            sumCountMap.put(treeSum, 1);
+        }
+        sumCountMap.put(Integer.MIN_VALUE, treeSum);
+        return sumCountMap;
+    }
+}
---
+++ src/test/java/org/willxu/algorithm/service/ints/MostFrequentSubtreeSumTest.java
@@ -0,0 +1,38 @@
+package org.willxu.algorithm.service.ints;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.impl.ints.MostFrequentSubtreeSumMap;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.gradle.internal.impldep.org.junit.Assert.assertArrayEquals;
+
+public class MostFrequentSubtreeSumTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                5, 2, -3
+                        ).collect(Collectors.toList())),
+                        new int[] {2, -3, 4}
+                ),
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                5, 2, -5
+                        ).collect(Collectors.toList())),
+                        new int[] {2}
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Map {index}")
+    @MethodSource("dataProvider")
+    public void testMap(TreeNode root, int[] excepted) {
+        MostFrequentSubtreeSum map = new MostFrequentSubtreeSumMap();
+        assertArrayEquals(excepted, map.findFrequentTreeSum(root));
+    }
+}
