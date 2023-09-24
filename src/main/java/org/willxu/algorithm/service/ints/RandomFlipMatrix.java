2bd444538d8ede4ffc6c20323fccf22fe323101a
Will Xu <xwywryx@outlook.com>
2023-09-24 21:54:57 +0800

    :time limit exceeded: 519. Random Flip Matrix

---
+++ src/main/java/org/willxu/algorithm/service/ints/RandomFlipMatrix.java
@@ -0,0 +1,29 @@
+package org.willxu.algorithm.service.ints;
+
+/**
+ * There is an m x n binary grid matrix with all the values set 0
+ * initially. Design an algorithm to randomly pick an index (i, j) where
+ * matrix[i][j] == 0 and flips it to 1. All the indices (i, j) where
+ * matrix[i][j] == 0 should be equally likely to be returned.
+ * <p>
+ * Optimize your algorithm to minimize the number of calls made to the
+ * built-in random function of your language and optimize the time and
+ * space complexity.
+ * <p>
+ * Implement the Solution class:
+ * <p>
+ * Solution(int m, int n) Initializes the object with the size of the
+ * binary matrix m and n.
+ */
+public interface RandomFlipMatrix {
+    /**
+     * @return a random index [i, j] of the matrix where
+     *         matrix[i][j] == 0 and flips it to 1.
+     */
+    int[] flip();
+
+    /**
+     * Resets all the values of the matrix to be 0.
+     */
+    void reset();
+}
