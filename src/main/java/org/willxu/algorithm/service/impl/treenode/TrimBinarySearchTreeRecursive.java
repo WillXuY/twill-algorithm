52e6d4d61c94e89dece7d3063cf2809798cb1a6d
Will Xu <xwywryx@outlook.com>
2023-11-27 17:46:21 +0800

    :sparkles: 669. Trim a Binary Search Tree

---
+++ src/main/java/org/willxu/algorithm/service/impl/treenode/TrimBinarySearchTreeRecursive.java
@@ -0,0 +1,21 @@
+package org.willxu.algorithm.service.impl.treenode;
+
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.treenode.TrimBinarySearchTree;
+
+public class TrimBinarySearchTreeRecursive implements TrimBinarySearchTree {
+    @Override
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = trimBST(root.left, low, high);
+        TreeNode right = trimBST(root.right, low, high);
+        if (root.val < low || root.val > high) {
+            return left == null ? right : left;
+        }
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
