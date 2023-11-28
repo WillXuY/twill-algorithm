4dbdd1ebde712b557215d3aaaec0bff81df1c9d2
Will Xu <xwywryx@outlook.com>
2023-11-28 23:10:15 +0800

    :sparkles: 72. Edit Distance

---
+++ src/main/java/org/willxu/algorithm/service/impl/integer/EditDistanceMatrix.java
@@ -0,0 +1,26 @@
+package org.willxu.algorithm.service.impl.integer;
+
+import org.willxu.algorithm.service.integer.EditDistance;
+
+public class EditDistanceMatrix implements EditDistance {
+    @Override
+    public int minDistance(String word1, String word2) {
+        int[][] matrix = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[0][i] = i;
+        }
+        for (int row = 1; row < matrix.length; row++) {
+            matrix[row][0] = row;
+            for (int col = 1; col < matrix[row].length; col++) {
+                int left = matrix[row][col - 1] + 1;
+                int top = matrix[row - 1][col] + 1;
+                int leftTop = matrix[row - 1][col - 1];
+                if (word1.charAt(row - 1) != word2.charAt(col - 1)) {
+                    leftTop++;
+                }
+                matrix[row][col] = Math.min(leftTop, Math.min(left, top));
+            }
+        }
+        return matrix[word1.length()][word2.length()];
+    }
+}
--- src/test/java/org/willxu/algorithm/service/integer/EditDistanceTest.java
+++ src/test/java/org/willxu/algorithm/service/integer/EditDistanceTest.java
@@ -1,8 +1,13 @@
 package org.willxu.algorithm.service.integer;
 
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.integer.EditDistanceMatrix;
 
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EditDistanceTest {
     static Stream<Arguments> dataProvider() {
@@ -10,5 +15,12 @@ public class EditDistanceTest {
                 Arguments.arguments("horse", "ros", 3),
                 Arguments.arguments("intention", "execution", 5)
         );
+    }
+
+    @ParameterizedTest(name = "Matrix {index}")
+    @MethodSource("dataProvider")
+    public void testMatrix(String word1, String word2, int excepted) {
+        EditDistance matrix = new EditDistanceMatrix();
+        assertEquals(excepted, matrix.minDistance(word1, word2));
     }
 }
