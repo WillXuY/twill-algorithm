7536e3fd04db97e1410efc6ca40737b2410c1c3f
Will Xu <xwywryx@outlook.com>
2023-12-14 22:15:01 +0800

    :sparkles: 712. Minimum ASCII Delete Sum for Two Strings

---
+++ src/main/java/org/willxu/algorithm/service/integer/MinimumAsciiDeleteSumForTwoStrings.java
@@ -0,0 +1,12 @@
+package org.willxu.algorithm.service.integer;
+
+public interface MinimumAsciiDeleteSumForTwoStrings {
+    /**
+     * Given two strings s1 and s2, return the lowest ASCII sum of
+     * deleted characters to make two strings equal.
+     *
+     * @param s1,s2 1 <= s1.length, s2.length <= 1000
+     *              consist of lowercase English letters.
+     */
+    int minimumDeleteSum(String s1, String s2);
+}
