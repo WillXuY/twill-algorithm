037181ab5b67610be0722e39f9ddf862f611f95f
Will Xu <xwywryx@outlook.com>
2023-12-16 23:05:22 +0800

    :sparkles: 714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ src/main/java/org/willxu/algorithm/service/integer/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -0,0 +1,25 @@
+package org.willxu.algorithm.service.integer;
+
+public interface BestTimeToBuyAndSellStockWithTransactionFee {
+    /**
+     * You are given an array prices where prices[i] is the price of a
+     * given stock on the i^th day, and an integer fee representing a
+     * transaction fee.
+     * <p>
+     * Find the maximum profit you can achieve. You may complete as many
+     * transactions as you like, but you need to pay the transaction fee
+     * for each transaction.
+     * <p>
+     * Note:
+     * <p>
+     * - You may not engage in multiple transactions simultaneously
+     *   (i.e., you must sell the stock before you buy again).
+     * - The transaction fee is only charged once for each stock
+     *   purchase and sale.
+     *
+     * @param prices 1 <= prices.length <= 5 * 10^4
+     *               1 <= prices[i] < 5 * 10^4
+     * @param fee 0 <= fee < 5 * 10^4
+     */
+    int maxProfit(int[] prices, int fee);
+}
