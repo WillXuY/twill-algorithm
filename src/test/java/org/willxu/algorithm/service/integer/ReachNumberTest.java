a8c2d0084337c73326f5f89cc0a838b7e7cba6e8
Will Xu <xwywryx@outlook.com>
2023-12-30 23:27:51 +0800

    :Time Limit Exceeded: 754. Reach a Number

---
+++ src/test/java/org/willxu/algorithm/service/integer/ReachNumberTest.java
@@ -0,0 +1,28 @@
+package org.willxu.algorithm.service.integer;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.service.impl.integer.ReachNumberSet;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReachNumberTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                // Time Limit Exceeded.
+                Arguments.arguments(-1000000000, 44723),
+                Arguments.arguments(2, 3),
+                Arguments.arguments(3, 2)
+        );
+    }
+
+    @ParameterizedTest(name = "Set {index}")
+    @MethodSource("dataProvider")
+    public void testSet(int target, int excepted) {
+        ReachNumber set = new ReachNumberSet();
+        assertEquals(excepted, set.reachNumber(target));
+    }
+}
