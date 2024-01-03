2d35e90d44239da20b65f48b9f954a4dd81d1413
Will Xu <xwywryx@outlook.com>
2024-01-03 23:29:43 +0800

    :sparkles: 767. Reorganize String

---
+++ src/main/java/org/willxu/algorithm/service/string/ReorganizeString.java
@@ -0,0 +1,15 @@
+package org.willxu.algorithm.service.string;
+
+public interface ReorganizeString {
+    /**
+     * Given a string s, rearrange the characters of s so that any two
+     * adjacent characters are not the same.
+     * <p>
+     * Return any possible rearrangement of s or return "" if not
+     * possible.
+     *
+     * @param s 1 <= s.length <= 500
+     *          consists of lowercase English letters.
+     */
+    String reorganizeString(String s);
+}
