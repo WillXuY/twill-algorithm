2e0da359915e5bbc80422886c93cdd3efe5d3991
Will Xu <xwywryx@outlook.com>
2024-02-02 17:51:13 +0800

    :sparkles: 814. Binary Tree Pruning

---
+++ src/test/java/org/willxu/algorithm/service/treenode/BinaryTreePruningTest.java
@@ -0,0 +1,50 @@
+package org.willxu.algorithm.service.treenode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.willxu.algorithm.provide.TreeNode;
+import org.willxu.algorithm.service.impl.treenode.BinaryTreePruningRecursive;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinaryTreePruningTest {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                1, null, 0, 0, 1
+                        ).collect(Collectors.toList())),
+                        TreeNode.initByListLevel(Stream.of(
+                                1, null, 0, null, 1
+                        ).collect(Collectors.toList()))
+                ),
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                1, 0, 1, 0, 0, 0, 1
+                        ).collect(Collectors.toList())),
+                        TreeNode.initByListLevel(Stream.of(
+                                1, null, 1, null, 1
+                        ).collect(Collectors.toList()))
+                ),
+                Arguments.arguments(
+                        TreeNode.initByListLevel(Stream.of(
+                                1, 1, 0, 1, 1, 0, 1, 0
+                        ).collect(Collectors.toList())),
+                        TreeNode.initByListLevel(Stream.of(
+                                1, 1, 0, 1, 1, null, 1
+                        ).collect(Collectors.toList()))
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "Recursive {index}")
+    @MethodSource("dataProvider")
+    public void testRecursive(TreeNode root, TreeNode excepted) {
+        BinaryTreePruning recursive = new BinaryTreePruningRecursive();
+        assertEquals(excepted, recursive.pruneTree(root));
+    }
+}
