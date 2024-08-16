03b55119912fff23b114512dd8631ac90bbb0d35
Will Xu <xwywryx@outlook.com>
2024-08-16 23:38:56 +0800

    :sparkles: 1081. Smallest Subsequence of Distinct Characters

---
+++ src/main/java/org/willxu/algorithm/service/string/SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,13 @@
+package org.willxu.algorithm.service.string;
+
+public interface SmallestSubsequenceOfDistinctCharacters {
+    /**
+     * Given a string s, return the lexicographically smallest
+     * subsequence of s that contains all the distinct characters of s
+     * exactly once.
+     *
+     * @param s 1 <= s.length <= 1000
+     *          s consists of lowercase English letters.
+     */
+    String smallestSubsequence(String s);
+}
