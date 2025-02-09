588c2a7ccda404d9f3294dfcbb9d04d0609def07
Will Xu <xwywryx@outlook.com>
2025-02-09 23:56:27 +0800

    :sparkles: 1530. Number of Good Leaf Nodes Pairs

---
+++ src/main/java/org/willxu/algorithm/service/integer/NumberOfGoodLeafNodesPairs.java
@@ -0,0 +1,17 @@
+package org.willxu.algorithm.service.integer;
+
+public interface NumberOfGoodLeafNodesPairs {
+	/**
+	 * You are given the root of a binary tree and an integer distance.
+	 * A pair of two different leaf nodes of a binary tree is said to be
+	 * good if the length of the shortest path between them is less than
+	 * or equal to distance.
+	 * <p>
+	 * Return the number of good leaf node pairs in the tree.
+	 * 
+	 * @param root The number of nodes in the tree is in the range [1, 2^10].
+	 *             1 <= Node.val <= 100
+	 * @param distance 1 <= distance <= 10
+	 */
+int countPairs(TreeNode root, int distance);
+}
