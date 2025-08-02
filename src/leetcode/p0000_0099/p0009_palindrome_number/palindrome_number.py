c056920087b539d4df256c3138e449f51b0171bb
Will Xu <xwywryx@outlook.com>
2025-08-02 23:32:31 +0800

    :sparkles: fix python p0009

--- src/leetcode/p0000_0099/p0009_palindrome_number/palindrome_number.py
+++ src/leetcode/p0000_0099/p0009_palindrome_number/palindrome_number.py
@@ -18,4 +18,12 @@ class PalindromeNumber(ABC):
 
 class PalindromeNumberArray(PalindromeNumber):
     def is_palindrome(self, x: int) -> bool:
-        pass
+        if x == 0:
+            return True;
+        if x < 0 or x % 10 == 0:
+            return False
+        reversed_num = 0
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+        return x == reversed_num or x == reversed_num // 10
