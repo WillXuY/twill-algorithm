68698d493507695ea8ee30657851ac9a5e471f43
Will Xu <xwywryx@outlook.com>
2025-08-12 23:50:46 +0800

    :sparkles: p0014 longest common prefix.

---
+++ src/leetcode/p0000_0099/p0014_longest_common_prefix/longest_common_prefix.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class LongestCommonPrefix(ABC):
+    """
+    Write a function to find the longest common prefix string amongst an array of strings.
+
+    If there is no common prefix, return an empty string "".
+    """
+    @abstractmethod
+    def longest_common_prefix(self, strs: List[str]) -> str:
+        """
+        Args:
+            1 <= strs.length <= 200
+            0 <= strs[i].length <= 200
+            strs[i] consists of only lowercase English letters if it is non-empty.
+
+        """
+        pass
+
+
+class LongestCommonPrefix1(LongestCommonPrefix):
+    def longest_common_prefix(self, strs:List[str]) -> str:
+        pass
+        return ""
