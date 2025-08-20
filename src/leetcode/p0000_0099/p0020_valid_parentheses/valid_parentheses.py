2845d067f12773bb928fd4f7f07c709ad370fbf7
Will Xu <xwywryx@outlook.com>
2025-08-20 22:59:01 +0800

    :sparkles: p0020 Valid Patentheses py.

---
+++ src/leetcode/p0000_0099/p0020_valid_parentheses/valid_parentheses.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class ValidParentheses(ABC):
+    """
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    An input string is valid if:
+        1. Open brackets must be closed by the same type of brackets.
+        2. Open brackets must be closed in the correct order.
+        3. Every close bracket has a corresponding open bracket of the same type.
+    """
+    @abstractmethod
+    def is_valid(self, s: str) -> bool:
+        """
+        Args:
+            - 1 <= s.length <= 10^4
+            - s consists of parentheses only '()[]{}'.
+        """
+        pass
+
+
+class ValidPatenthesesStack(ValidPatentheses):
+    def is_valid(self, s: str) -> bool:
+        pass
+        return False
