3c7fa80fb43808ca9358a0c44c600319780d740b
Will Xu <xwywryx@outlook.com>
2025-08-25 23:28:49 +0800

    :sparkles: Recursive save 21.merge two lists.

--- src/leetcode/p0000_0099/p0021_merge_two_sorted_lists/merge_two_sorted_lists.py
+++ src/leetcode/p0000_0099/p0021_merge_two_sorted_lists/merge_two_sorted_lists.py
@@ -23,7 +23,16 @@ class MergeTwoSortedLists(ABC):
         """
         pass
 
-
-class MergeTwoSortedLists1(MergeTwoSortedLists):
+class MergeTwoSortedListsRecursive(MergeTwoSortedLists):
     def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val <= list2.val:
+            list1.next = self.merge_two_lists(list1.next, l2)
+            return list1
+        else:
+            list2.next = self.merge_two_lists(list1, list2.next)
+            return list2
         return ListNode(0)
