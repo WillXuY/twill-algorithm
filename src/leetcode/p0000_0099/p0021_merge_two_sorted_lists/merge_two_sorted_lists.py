4fb76c2cd6aa50de2bb49b6d7d9052107e9c9498
Will Xu <xwywryx@outlook.com>
2025-08-28 22:32:24 +0800

    :bug: fix param name problem.

--- src/leetcode/p0000_0099/p0021_merge_two_sorted_lists/merge_two_sorted_lists.py
+++ src/leetcode/p0000_0099/p0021_merge_two_sorted_lists/merge_two_sorted_lists.py
@@ -30,7 +30,7 @@ class MergeTwoSortedListsRecursive(MergeTwoSortedLists):
         if not list2:
             return list1
         if list1.val <= list2.val:
-            list1.next = self.merge_two_lists(list1.next, l2)
+            list1.next = self.merge_two_lists(list1.next, list2)
             return list1
         else:
             list2.next = self.merge_two_lists(list1, list2.next)
