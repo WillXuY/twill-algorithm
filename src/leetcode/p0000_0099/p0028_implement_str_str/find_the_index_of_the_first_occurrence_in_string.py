66eecc9783c71e866097ebb40239eafd100d0a2c
Will Xu <xwywryx@outlook.com>
2025-09-09 23:41:54 +0800

    :sparkles: 28. Find the index of the first occurrence in a string.

---
+++ src/leetcode/p0000_0099/p0028_implement_str_str/find_the_index_of_the_first_occurrence_in_string.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class FindTheIndexOfTheFirstOccurrenceInString(ABC):
+    """
+    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+    """
+    @abstractmethod
+    def strStr(self, haystack: str, needle: str) -> int:
+        """
+        Args:
+            haystack, needle:
+                - 1 <= haystack.length, needle.length <= 10^4
+                - haystack and needle consist of only lowercase English characters.
+
+        """
+        pass
