2aba3b6aceed58d86ec77616867342cfe1feaa7f
Will Xu <xwywryx@outlook.com>
2025-09-10 23:14:42 +0800

    :sparkles: brute force solve 28.

--- src/leetcode/p0000_0099/p0028_implement_str_str/find_the_index_of_the_first_occurrence_in_string.py
+++ src/leetcode/p0000_0099/p0028_implement_str_str/find_the_index_of_the_first_occurrence_in_string.py
@@ -14,3 +14,13 @@ class FindTheIndexOfTheFirstOccurrenceInString(ABC):
 
         """
         pass
+
+class FindTheIndexOfTheFirstOccurrenceInStringBruteForce(FindTheIndexOfTheFirstOccurrenceInString):
+    def strStr(self, haystack: str, needle: str) -> int:
+        n, m = len(haystack), len(needle)
+        if m == 0:
+            return 0
+        for i in range(n - m + 1):
+            if haystack[i:i+m] == needle:
+                return i
+        return -1
