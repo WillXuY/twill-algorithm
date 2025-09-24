bafdae2157c5b63004576d229436590d2fc1c108
Will Xu <xwywryx@outlook.com>
2025-09-24 22:34:26 +0800

    :sparkles: loop solve add binary.

--- src/leetcode/p0000_0099/p0067_add_binary/add_binary.py
+++ src/leetcode/p0000_0099/p0067_add_binary/add_binary.py
@@ -16,3 +16,21 @@ class AddBinary(ABC):
                 consist only of '0' or '1' characters
         """
         pass
+
+class AddBinaryLoop(AddBinary):
+    def add_binary(self, a: str, b: str) -> str:
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            result.append(str(total % 2))
+            carry = total // 2
+        return ''.join(reversed(result))
