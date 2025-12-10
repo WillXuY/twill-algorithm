f4f4c7c5e72e1cce046d67f365ee9a3fc80fa758
Will Xu <xwywryx@outlook.com>
2025-12-10 23:54:52 +0800

    :sparkles: 231. Power of Two.

---
+++ src/leetcode/p0200_0299/p0231_power_of_two/power_of_two.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class PowerOfTwo(ABC):
+    """
+    Given an integer n, return true if it is a power of two. Otherwise, return false.
+    An integer n is a power of two, if there exists an integer x such that n == 2^x.
+    """
+    @abstractmethod
+    def isPowerOfTwo(self, n: int) -> bool:
+        """
+        Args:
+            n:
+                -2^31 <= n <= 2^31 - 1
+        """
+        pass
